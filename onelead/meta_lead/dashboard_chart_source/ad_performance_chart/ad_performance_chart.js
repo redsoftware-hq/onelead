d486bbd651b32f1b1ab39d6ba9fba877f707c11f
krushit dudhat <krushit.dudhat@redsoftware.in>
2025-03-28 19:39:56 +0000

    fix: no of ads in chart control via filter

--- onelead/meta_lead/dashboard_chart_source/ad_performance_chart/ad_performance_chart.js
+++ onelead/meta_lead/dashboard_chart_source/ad_performance_chart/ad_performance_chart.js
@@ -5,8 +5,14 @@ frappe.dashboards.chart_sources["Ad Performance Chart"] = {
       fieldname: "processing_status",
       label: __("Processing Status"),
       fieldtype: "Select",
-      options: "\nProcessed\nError\nPending\nUnconfigured\nDisabled",
+      options: "\nProcessed\nError\nUnconfigured",
       default: "Processed"
+    },
+    {
+      fieldname: "no_of_ads",
+      label: __("No of Ads"),
+      fieldtype: "Int",
+      default: 15
     }
   ]
 }
--- onelead/meta_lead/dashboard_chart_source/ad_performance_chart/ad_performance_chart.py
+++ onelead/meta_lead/dashboard_chart_source/ad_performance_chart/ad_performance_chart.py
@@ -5,7 +5,8 @@ from collections import defaultdict
 def get(chart_name=None, chart=None, no_cache=None, filters=None, from_date=None, to_date=None, timespan=None, time_interval=None, heatmap_year=None):
     filters = frappe.parse_json(filters) if filters else {}
     status_filter = filters.get("processing_status") or "Processed"
-    limit = 15  # Top 15 ads
+    limit = filters.get("no_of_ads") or 15
+    # limit = 15  # Top 15 ads
 
     # Join Meta Webhook Lead Logs with Ads to get ad_name
     data = frappe.db.sql("""
